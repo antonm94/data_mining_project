ae63ccbdd5f87f6fa99232056e093762b961e448
Dal Noguer <[dal.noguer@gmail.com]>
2017-12-02 10:33:48 +0100

    k-means

--- task3/example.py
+++ task3/example.py
@@ -1,58 +1,13 @@
-Skip to content
-This repository
-Search
-Pull requests
-Issues
-Marketplace
-Explore
- @antonm94
- Sign out
- Watch 1
-  Star 0  Fork 0 albertomontesg/data-mining-project
- Code  Issues 0  Pull requests 0  Projects 0  Wiki  Insights
-Branch: master Find file Copy pathdata-mining-project/task3/mapreduce.py
-b8482ac  on 12 Dec 2016
-@albertomontesg albertomontesg Submit final version of the implementation
-1 contributor
-RawBlameHistory     
-178 lines (134 sloc)  5.4 KB
-import logging
-
 import numpy as np
+from scipy.spatial.distance import cdist
+import logging
+import math
 
 logger = logging.getLogger(__name__)
 
-DEBUG = 2
-N_CLUSTERS = 200
-N_INIT = 20
-N_CORESETS = 500
-NOTES = """
-For sampling the Coresets centers, used the D^2 sampling criteria.
-The initialization of the kmeans algorithm is using uniformly sampling.
-The best results have been obtained increasing the number of coresets return by each mapper at
-least to 400 coresets per mapper.
-"""
-
-print('\n' + '#'*40 + '\n')
-print('Number of clusters:\t\t\t{}'.format(N_CLUSTERS))
-print('Number of initializations:\t{}'.format(N_INIT))
-print('Number of coresets:\t\t\t{}'.format(N_CORESETS))
-print('Notes: {}'.format(NOTES))
-print('\nResult:')
-
-np.random.seed(23)
-
-def euclidean_distance(X, Y):
-    """ Compute the euclidean distance pair-wise between column vectors of X and Y """
-    if len(Y.shape) == 1:
-        return np.sum((X-Y)**2, axis=1)
-
-    assert X.shape[1] == Y.shape[1], 'Last dimension do not match'
-
-    result = np.zeros((X.shape[0], Y.shape[0]))
-    for i in range(X.shape[0]):
-        result[i,:] = euclidean_distance(Y, X[i,:])
-    return result
+N_CORESETS = 4 # Needs to be a power of 2 for correct tree
+CORESET_SIZE = 300
+N_CLUSTERS = 200 # Specified by problem description
 
 def init_centers_d2_sampling(X, n_clusters):
     """ D^2 sampling algorithm to find a proper cluster centers given a dataset of points """
@@ -81,21 +36,66 @@ def init_centers_d2_sampling(X, n_clusters):
         # Compute the probability distribution normalizing the squared distance
         p = min_dist / min_dist.sum()
 
-        if DEBUG > 2:
-            logger.info('Sampled center {}'.format(i))
-
-    if DEBUG > 1:
-        logger.info('Finish D^2 sampling')
-
     return centers
 
-def init_centers_random_unif(X, n_clusters):
-    """ Initialize choosing randomly samples as centers """
-    n_samples = X.shape[0]
+def sample_coreset(X,n_clusters):
+    """
+    :param X: data set to be compressed
+    :param n_clusters: dimension of the reduced dataset
+    :return: new reduced dataset with corresponding weight per point
+    """
+    n_b,d = X.shape
+    coreset = np.empty((n_clusters,d),dtype=float)
+    weights = np.empty((n_clusters,1),dtype=float)
 
-    idx = np.random.permutation(n_samples)[:n_clusters]
-    centers = X[idx,:]
-    return centers
+    # Step 1: D^2 Sampling (rough approximation)
+    B = init_centers_d2_sampling(X,n_clusters)
+
+    # Step 2: Sample coreset using importance sampling
+    # Construct vector q(x) to sample
+    dist = cdist(X,B)
+    squared_dist = np.min(dist,axis=1)**2
+    cluster_membership = np.argmin(dist,axis=1)
+    c_phi = np.mean(squared_dist)
+    alpha = 1
+    # First term of q(x)
+    first_term = alpha*squared_dist/c_phi
+    # Second term of q(x) (might be slow due to for loop)
+    B_i = []
+    B_i_array = np.zeros((n_b,n_b),dtype=int)
+    for i in range(n_clusters):
+        B_i.append(np.where(cluster_membership==i))
+    for i in range(n_b):
+        B_i_array[i,B_i[cluster_membership[i]]] = 1
+    second_term = 2*alpha/c_phi*np.divide(np.dot(B_i_array,squared_dist),np.sum(B_i_array,axis=1))
+    # Third term of q(x)
+    third_term = 4*n_b/np.sum(B_i_array,axis=1)
+    q = first_term + second_term + third_term
+    q = q/q.sum()
+
+    # Sample from q(x)
+    indexes = np.arange(n_b)
+    for i in range(n_clusters):
+        idx = np.random.choice(indexes, p=q)
+        coreset[i,:] = X[idx,:]
+        weights[i,0] = 1/q[idx]
+
+    # Stack cluster centers with weights
+    C = np.hstack((weights,coreset))
+
+    return C
+
+def euclidean_distance(X, Y):
+    """ Compute the euclidean distance pair-wise between column vectors of X and Y """
+    if len(Y.shape) == 1:
+        return np.sum((X-Y)**2, axis=1)
+
+    assert X.shape[1] == Y.shape[1], 'Last dimension do not match'
+
+    result = np.zeros((X.shape[0], Y.shape[0]))
+    for i in range(X.shape[0]):
+        result[i,:] = euclidean_distance(Y, X[i,:])
+    return result
 
 def kmeans_coresets(X, w, n_clusters=8, n_init=10, max_iter=300, tol=.0001):
     """ Fit the K-Means cluster algorithm with the coresets represented by the points `X` and
@@ -111,7 +111,7 @@ def kmeans_coresets(X, w, n_clusters=8, n_init=10, max_iter=300, tol=.0001):
     for i in range(n_init):
 
         # Initialize the centers using the k-means++ algorithm
-        centers = init_centers_random_unif(X, n_clusters)
+        centers = init_centers_d2_sampling(X, n_clusters)
 
         it = 0
         prev_L = 0
@@ -129,7 +129,6 @@ def kmeans_coresets(X, w, n_clusters=8, n_init=10, max_iter=300, tol=.0001):
             # Update
             for l in range(n_clusters):
                 if np.sum(labels==l) == 0:
-                    logger.warning('No labels of {}'.format(l))
                     continue
                 P = X[labels==l,:]
                 pw = w[labels==l,:]
@@ -140,14 +139,11 @@ def kmeans_coresets(X, w, n_clusters=8, n_init=10, max_iter=300, tol=.0001):
                 break
             prev_L = L
 
-            if DEBUG >= 2:
-                logger.info('Iteration {}\tInertia: {:.5f}'.format(it, L))
             it += 1
 
-        if it == max_iter:
-            logger.warning('Maximum iteration reached')
-        elif DEBUG >= 1:
-            logger.info('Finished initialization {} with {} iterations and intertia {:.5f}'.format(i, it, L))
+        logger.info('Finished with {} iterations!'.format(it))
+
+
         # Compute intertia and update the best parameters
         inertia = L
         if best_inertia is None or inertia < best_inertia:
@@ -157,31 +153,50 @@ def kmeans_coresets(X, w, n_clusters=8, n_init=10, max_iter=300, tol=.0001):
 
     return best_centers
 
-
 def mapper(key, value):
+    """
     # key: None
     # value: one line of input file
-    assert key is None, 'key is not None'
-
-    # To compute the coreset first uniformly sample over all the points.
-    # Then compute the weight of each sample as the number of samples closer to each one
+    For each batch of data:
+        1. Extract 128 coresets.
+        2. Union and compression until just 1 coreset.
+        3. Yield coreset
+    """
+    # Get dimensions
     X = value
-    nb_samples = X.shape[0]
+    n_b = X.shape[0]
+    d = X.shape[1]
 
-    # Sample points for coresets
-    c = init_centers_d2_sampling(X, N_CORESETS)
 
-    w = np.zeros((N_CORESETS, 1))
-    for i in range(nb_samples):
-        d = euclidean_distance(c, X[i])
-        w[np.argmin(d), 0] += 1
 
-    C = np.hstack([w, c])
+    # Create first row of coresets
+    coresets = {}
+    n_levels = int(math.log(N_CORESETS,2))
+    for i in range(1,n_levels+1):
+        coresets[i] = []
+        for j in range((N_CORESETS)/(2**(i-1))):
+            if i == 1:
+                # Sample coresets
+                coresets[i].append(sample_coreset(X,CORESET_SIZE))
+            else:
+                c1 = coresets[i-1].pop()[:,1:]
+                c2 = coresets[i-1].pop()[:,1:]
+                c = np.vstack((c1,c2))
+                coresets[i].append(sample_coreset(c,CORESET_SIZE))
 
-    if DEBUG >= 1:
-        logger.info('Finished mapper')
-    yield 'w', C
+    c1 = coresets[n_levels].pop()[:, 1:]
+    c2 = coresets[n_levels].pop()[:, 1:]
+    c = np.vstack((c1, c2))
 
+    C = sample_coreset(c,CORESET_SIZE)
+
+
+
+    # Just sample some coresets and yield them together with the weight
+    # Train it in reducing phase.
+
+    logger.info('Finished mapper')
+    yield 'w', C  # this is how you yield a key, value pair
 
 def reducer(key, values):
     # key: key from mapper used to aggregate
@@ -189,7 +204,10 @@ def reducer(key, values):
     # Note that we do *not* output a (key, value) pair here.
     assert key == 'w', 'Key is has not the correct value'
 
-    w, X = values[:,0].reshape(-1, 1), values[:,1:]
-    cluster_centers = kmeans_coresets(X, w, N_CLUSTERS, N_INIT)
+    logger.info('Starting reducer')
 
+    w, X = values[:,0].reshape(-1, 1), values[:,1:].reshape(-1,250)
+    cluster_centers = kmeans_coresets(X, w, N_CLUSTERS, 3)
+
+    logger.info('Finished reducer')
     yield cluster_centers
