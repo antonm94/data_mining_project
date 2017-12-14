4c9af2fcc7638910959ad82483e6439aecf84e2e
antonm94 <manton@student.ethz.ch>
2017-12-14 13:34:12 +0100

    added task4

---
+++ task4/example.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+def set_articles(articles):
+    timestamps = articles.keys()
+    user_features = np.zeros((len(timestamps), 6))
+    available_articles = np.empty(len(timestamps), object)
+    for i in range(len(timestamps)):
+        user_features[i] = articles.get(timestamps[i])[0:5]
+        print articles.get(timestamps[i])[6:]
+    print user_features
+
+
+
+def update(reward):
+    pass
+
+
+def recommend(time, user_features, choices):
+    return np.random.choice(choices)
