e4780ac69d3db37b6e3e1f1472023dff44aaafa2
antonm94 <manton@student.ethz.ch>
2017-12-14 13:54:56 +0100

    added files

--- task4/example.py
+++ task4/example.py
@@ -1,13 +1,15 @@
 import numpy as np
 
 def set_articles(articles):
-    timestamps = articles.keys()
-    user_features = np.zeros((len(timestamps), 6))
-    available_articles = np.empty(len(timestamps), object)
-    for i in range(len(timestamps)):
-        user_features[i] = articles.get(timestamps[i])[0:5]
-        print articles.get(timestamps[i])[6:]
-    print user_features
+    article_id = articles.keys()
+
+     # timestamps = articles.keys()
+    # user_features = np.zeros((len(timestamps), 6))
+    # available_articles = np.empty(len(timestamps), object)
+    # for i in range(len(timestamps)):
+    #     user_features[i] = articles.get(timestamps[i])[0:5]
+    #     print articles.get(timestamps[i])[6:]
+    # print user_features
 
 
 
