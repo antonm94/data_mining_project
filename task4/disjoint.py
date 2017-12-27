de64106efc6d0a9646ddc7a2d7133eed5023dc60
Marc Heim <heimm@ethz.ch>
2017-12-27 12:46:48 +0100

    added fast disjoint algorithm

--- task4/disjoint.py
+++ task4/disjoint.py
@@ -4,19 +4,19 @@ import numpy as np
 from numpy import linalg
 import time
 
-method = 'HybridUCB'
+method = 'disjointUCB'
 
-class HybridUCB:
+class disjointUCB:
     def __init__(self):
         self.article_features = {}
 
         # upper bound coefficient
-        self.alpha = 3 #1 + np.sqrt(np.log(2/delta)/2)
-        r1 = 20.
+        self.alpha = 2.9 #1 + np.sqrt(np.log(2/delta)/2)
+        r1 = 20
         r0 = -0.5
         self.r = (r0, r1)
         # dimension of user features = d
-        self.d = 5
+        self.d = 6
         # dimension of article features = k
         self.k = self.d*self.d
         # A0 : matrix to compute hybrid part, k*k
@@ -72,28 +72,17 @@ class HybridUCB:
         self.article_features = np.zeros((art_len, 1, self.d))
         self.Aa = np.zeros((art_len, self.d, self.d))
         self.AaI = np.zeros((art_len, self.d, self.d))
-        self.Ba = np.zeros((art_len, self.d, self.k))
-        self.BaT = np.zeros((art_len, self.k, self.d))
         self.ba = np.zeros((art_len, self.d, 1))
         self.AaIba = np.zeros((art_len, self.d, 1))
         self.AaIBa = np.zeros((art_len, self.d, self.k))
-        self.BaTAaI = np.zeros((art_len, self.k, self.d))
-        self.A0IBaTAaI = np.zeros((art_len, self.k, self.d))
-        self.A0IBaA0IBaTAaI = np.zeros((art_len, self.d, self.d))
         self.theta = np.zeros((art_len, self.d, 1))
         for key in art:
             self.index[key] = i
             self.article_features[i] = art[key]
             self.Aa[i] = np.identity(self.d)
             self.AaI[i] = np.identity(self.d)
-            self.Ba[i] = np.zeros((self.d, self.k))
-            self.BaT[i] = np.zeros((self.k, self.d))
             self.ba[i] = np.zeros((self.d, 1))
             self.AaIba[i] = np.zeros((self.d, 1))
-            self.AaIBa[i] = np.zeros((self.d, self.k))
-            self.BaTAaI[i] = np.zeros((self.k, self.d))
-            self.A0IBaTAaI[i] = np.zeros((self.k, self.d))
-            self.A0IBaA0IBaTAaI[i] = np.zeros((self.d, self.d))
             self.theta[i] = np.zeros((self.d, 1))
             i += 1
 
@@ -107,52 +96,17 @@ class HybridUCB:
              pass
         else:
             r = self.r[reward]
-            self.A0 += np.dot(self.BaTAaI[self.a_max], self.Ba[self.a_max])
-            self.b0 += np.dot(self.BaTAaI[self.a_max], self.ba[self.a_max])
             self.Aa[self.a_max] += np.dot(self.xa, self.xaT)
             B = np.divide(
                 np.dot(np.dot(self.AaI[self.a_max], np.dot(self.xa, self.xaT)), self.AaI[self.a_max]),
                 (1 + np.dot(np.dot(self.xaT, self.AaI[self.a_max]), self.xa)))
 
             self.AaI[self.a_max] = self.AaI[self.a_max] - B
-            self.Ba[self.a_max] += np.dot(self.xa, self.zT)
-            self.BaT[self.a_max] = np.transpose(self.Ba[self.a_max])
             self.ba[self.a_max] += r * self.xa
             self.AaIba[self.a_max] = np.dot(self.AaI[self.a_max], self.ba[self.a_max])
-            self.AaIBa[self.a_max] = np.dot(self.AaI[self.a_max], self.Ba[self.a_max])
-            self.BaTAaI[self.a_max] = np.dot(self.BaT[self.a_max], self.AaI[self.a_max])
-
-
-
-            self.A0 += np.dot(self.z, self.zT) - np.dot(self.BaTAaI[self.a_max], self.Ba[self.a_max])
-            self.b0 += r * self.z - np.dot(self.BaT[self.a_max], np.dot(self.AaI[self.a_max], self.ba[self.a_max]))
-            #change to LSG?
-
-            self.A0I = linalg.inv(self.A0)
-            self.beta = np.dot(self.A0I, self.b0)
-            #self.beta = linalg.solve(self.A0, self.b0)
-            # if not np.array_equal(beta1, self.beta.all):
-            #     print
-
-            self.A0IBaTAaI[self.a_max] = np.dot(self.A0I,self.BaTAaI[self.a_max])
-            self.A0IBaA0IBaTAaI[self.a_max] = np.dot(self.AaIBa[self.a_max],self.A0IBaTAaI[self.a_max])
-            #do this here !!!!!
-            #self.lastUp = self.lastUp + 1
-            #if self.lastUp > self.updateCycle:
-                 #self.matrixUpdate()
-                 #self.lastUp = 0
             
-            self.theta = self.AaIba - np.dot(self.AaIBa, self.beta)#self.AaI[article].dot(self.ba[article] - self.Ba[article].dot(self.beta))
+            self.theta = self.AaIba - np.dot(self.AaIBa, self.beta)
             self.lastart = []
-
-    def matrixUpdate(self):
-          #global ttime
-          #ttime = ttime - time.time()
-          for ind in range(self.BaTAaI.shape[0]):
-                #print self.A0IBaTAaI
-                self.A0IBaTAaI[ind] = np.dot(self.A0I,self.BaTAaI[ind])
-                self.A0IBaA0IBaTAaI[ind] = np.dot(self.AaIBa[ind],self.A0IBaTAaI[ind])
-          #ttime = ttime + time.time()
 
 
     # This function will be called by the evaluator.
@@ -174,42 +128,19 @@ class HybridUCB:
             self.article_features_tmp = self.article_features[index]
 
             #preprocess matrices
-            self.preA0IBaA0IBaTAaI=self.A0IBaA0IBaTAaI[index]
             self.preAaI = self.AaI[index]
-            self.preA0IBaTAaI = self.A0IBaTAaI[index]
-            self.preTheta = self.theta[index]
             self.lastart = index
+            self.preTheta = self.theta[index]
             
         ttime = ttime + time.time()
+        AaIxa = np.dot(self.preAaI, self.xa)
 
-        zaT_tmp = np.einsum('i,j', self.article_features_tmp.reshape(-1), user_features).reshape(article_len, 1, self.k)
-
-	#print zaT_tmp.shape
-
-        za_tmp = np.transpose(zaT_tmp, (0,2,1))#np.transpose(zaT_tmp,(0,2,1))
-        A0IBaTAaIxa_tmp = np.dot(self.preA0IBaTAaI, self.xa)
-        #looks good :)
-        #print A0IBaTAaIxa_tmp1-A0IBaTAaIxa_tmp
-
-        A0Iza_tmp = np.transpose(np.dot(zaT_tmp, np.transpose(self.A0I)), (0,2,1)) # (20, 36, 1)
-        A0Iza_diff_2A0IBaTAaIxa_tmp = A0Iza_tmp - 2*A0IBaTAaIxa_tmp
-
-        sa_1_tmp = np.sum(za_tmp.reshape(article_len,self.k,1,1)*A0Iza_diff_2A0IBaTAaIxa_tmp.reshape(article_len, self.k,1,1),-3)
-
-        #ttime = ttime - time.time()
-        AaIxa_add_AaIBaA0IBaTAaIxa_tmp = np.dot(self.preAaI, self.xa) + np.dot(self.preA0IBaA0IBaTAaI,self.xa)
-        #ttime = ttime + time.time()
-	#print AaIxa_add_AaIBaA0IBaTAaIxa_tmp - AaIxa_add_AaIBaA0IBaTAaIxa_tmp1
-
-	sa_2_tmp = np.transpose(np.dot(np.transpose(AaIxa_add_AaIBaA0IBaTAaIxa_tmp,(0,2,1)),self.xa),(0,2,1))
-        sa_tmp = sa_1_tmp + sa_2_tmp
+	sa_tmp = np.transpose(np.dot(np.transpose(AaIxa,(0,2,1)),self.xa),(0,2,1))
 
         xaTtheta_tmp = np.transpose(np.dot(np.transpose(self.preTheta,(0,2,1)),self.xa),(0,2,1))
 
-        max_index = np.argmax(np.dot(zaT_tmp, self.beta) + xaTtheta_tmp + self.alpha * np.sqrt(sa_tmp))
+        max_index = np.argmax(xaTtheta_tmp + self.alpha * np.sqrt(sa_tmp))
 
-        self.z = za_tmp[max_index]
-        self.zT = zaT_tmp[max_index]
         art_max = index[max_index]
 
         # article index with largest UCB
@@ -221,7 +152,7 @@ class HybridUCB:
 
 
 algorithms = {
-    'HybridUCB': HybridUCB()
+    'disjointUCB': disjointUCB()
 }
 
 algorithm = algorithms[method]
